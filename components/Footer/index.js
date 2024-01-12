54a1d12520ededf261e74d3a6b4dd8d0b1f652d6
Jasmine <jdaniera@Jasmines-MacBook-Pro-2.local>
2024-01-11 22:17:15 -0800

    edited footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 export default function Footer() {
     return(
         <footer className={styles.footer}>
-            created by Jasmine in Monika's class xo
+            created by Jasmine Putri / Set H
         </footer>
     )
 }
